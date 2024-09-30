a10baa199927c41e771042ecc939b4f8899f05d4
harsha0603 <harshamassss@gmail.com>
2024-09-30 18:57:15 +0530

    model added

--- README.md
+++ README.md
@@ -63,8 +63,4 @@ dvc init
 dvc repro
 ```
 
-## User interface 
-![alt text](image.png)
 
-
-![alt text](image-1.png)
--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def predictRoute():
 
 if __name__ == "__main__":
     clApp = ClientApp()
-    app.run(host='127.0.0.1', port=8080) 
+    app.run(host='0.0.0.0', port=8080) #for AWS
